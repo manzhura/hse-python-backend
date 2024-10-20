5fbff5506570690bb3182a3ca50aed1824676edd
Ваше Имя <you@example.com>
2024-10-20 21:09:48 +0300

    hw4

--- tests/lecture_4/hw/test_service.py
+++ tests/lecture_4/hw/test_service.py
@@ -29,6 +29,17 @@ def user_info():
     )
 
 @pytest.fixture
+def admin_info():
+    return UserInfo(
+        username="admin",
+        name="admin",
+        birthdate=datetime.fromtimestamp(0.0),
+        role=UserRole.ADMIN,
+        password=SecretStr("superSecretAdminPassword123")
+
+    )
+
+@pytest.fixture
 def entity(user_info):
     return UserEntity(
         uid=1,
@@ -74,68 +85,91 @@ def test_register_user(client, user_info):
     assert response.json()["name"] == user_info.name
     assert response.json()["birthdate"] == user_info.birthdate.isoformat()
     assert response.json()["role"] == user_info.role
+    
+    # тест не корректного запроса
+    second_response = client.post("/user-register", json=registration_data)
+    assert second_response.status_code == HTTPStatus.BAD_REQUEST
 
 def test_password_reg(client, user_info):
     user_info.password = SecretStr("123")
     registration_data = user_info.model_dump()
     registration_data["password"] = user_info.password.get_secret_value()
     registration_data["birthdate"] = user_info.birthdate.isoformat()
-
+    
+    # тест не корректного запроса
     register_response = client.post("/user-register", json=registration_data)
     assert register_response.status_code == HTTPStatus.BAD_REQUEST
-
 
 def test_get_user_by_user(client, user_info, entity):
     registration_data = user_info.model_dump()
     registration_data["password"] = user_info.password.get_secret_value()
     registration_data["birthdate"] = user_info.birthdate.isoformat()
     credentials = (user_info.username, user_info.password.get_secret_value())
-
+    
+    # тест корректного получения user
     registration_response = client.post("/user-register", json=registration_data)
     user_id = registration_response.json()["uid"]
-    fetch_response = client.post("/user-get", params={'id': user_id}, auth=credentials)
+    fetch_response = client.post("/user-get", params={"id": user_id}, auth=credentials)
 
     assert fetch_response.status_code == HTTPStatus.OK
     assert fetch_response.json()["name"] == user_info.name
     assert fetch_response.json()["username"] == user_info.username
     assert fetch_response.json()["role"] == user_info.role
 
-
+    # тест не корректного запроса
     invalid_user_fetch_response = client.post(
         "/user-get",
-        params={'id': entity.uid, 'username': entity.info.username}, auth=credentials
+        params={"id": entity.uid, "username": entity.info.username}, auth=credentials
     )
     assert invalid_user_fetch_response.status_code == HTTPStatus.BAD_REQUEST
-
+    
     empty_params_response = client.post("/user-get", params={}, auth=credentials)
     assert empty_params_response.status_code == HTTPStatus.BAD_REQUEST
 
     wrong_password_auth = (user_info.username, "wrong_password")
     unauthorized_response = client.post(
         "/user-get",
-        params={'id': registration_response.json().get("uid")},
+        params={"id": registration_response.json().get("uid")},
         auth=wrong_password_auth
     )
     assert unauthorized_response.status_code == HTTPStatus.UNAUTHORIZED
 
-    non_existent_id_response = client.post("/user-get", params={'id': 9,'username': entity.info.username}, auth=credentials)
+    non_existent_id_response = client.post("/user-get", params={"id": 9,"username": entity.info.username}, auth=credentials)
     assert non_existent_id_response.status_code == HTTPStatus.BAD_REQUEST
 
-
-def test_promote(client, user_info):
+    non_credentials_response = client.post("/user-get", params={"id": user_id,  "username": entity.info.username})
+    assert non_credentials_response.json()["detail"] == "Not authenticated"
+    
+def test_promote(client, user_info, admin_info):
     registration_data = user_info.model_dump()
     registration_data["password"] = user_info.password.get_secret_value()
     registration_data["birthdate"] = user_info.birthdate.isoformat()
-    credentials = (user_info.username, user_info.password.get_secret_value())
-
-    register_response = client.post("/user-register", json=registration_data)
-
+    credentials = (user_info.username, user_info.password.get_secret_value())   
+    
+    
+    register_response = client.post("/user-register", json=registration_data)    
     assert register_response.status_code == HTTPStatus.OK
-
-    promote_response = client.post("/user-promote", params={'id': register_response.json()["uid"]}, auth=credentials)
+    user_id = register_response.json()["uid"]
+    promote_response = client.post("/user-promote", params={"id": user_id}, auth=credentials)
     assert promote_response.status_code == HTTPStatus.FORBIDDEN
 
+    # тестирование от админки
+    credentials_admin = (admin_info.username, admin_info.password.get_secret_value())
+    promote_response = client.post("/user-promote", params={"id": user_id}, auth=credentials_admin)
+    fetch_response = client.post("/user-get", params={"id": user_id}, auth=credentials_admin)
+    assert promote_response.status_code == HTTPStatus.OK
+    assert fetch_response.json()["role"] == "admin"
+
+    fetch_response = client.post("/user-get", params={"username": user_info.username}, auth=credentials_admin)
+    assert fetch_response.status_code == HTTPStatus.OK
+
+    fetch_response = client.post("/user-get", params={"username": "random_user"}, auth=credentials_admin)
+    assert fetch_response.status_code == HTTPStatus.NOT_FOUND
+
+    random_promote_response = client.post("/user-promote", params={"id": 999}, auth=credentials_admin)
+    assert random_promote_response.status_code == HTTPStatus.BAD_REQUEST
+
 def test_password_is_longer_than_8():
-    assert password_is_longer_than_8(SecretStr('123456798')) == True
-    assert password_is_longer_than_8(SecretStr('123')) == False
-    assert password_is_longer_than_8(SecretStr('')) == False
+    assert password_is_longer_than_8(SecretStr("123456798")) == True
+    assert password_is_longer_than_8(SecretStr("123")) == False
+    assert password_is_longer_than_8(SecretStr("")) == False
